5219d9e31ac7c04ef5d7bb9eed0c9efa5615f07a
jorgeisa <isaacxicol@gmail.com>
2023-01-04 15:13:05 -0600

    Update

--- src/ArbolDecision.py
+++ src/ArbolDecision.py
@@ -11,21 +11,20 @@ def arbolDecision_func(x_name, y_name, titulo, datos):
     y = datos[y_name].values.reshape(-1,1)
     x_array = []
     y_array = []
+
+    # Convirtiendo a listas las columnas
     for i in x:
         x_array.append(i[0])
 
     for j in y:
         y_array.append(j[0])
 
-    # Converting string labels into numbers.
+    # Codificando los datos de las listas
     le = preprocessing.LabelEncoder()
     x_encoded = le.fit_transform(x_array)
     y_encoded = le.fit_transform(y_array)
 
-    print("x_encoded: ", x_encoded)
-    print("y_encoded: ", y_encoded)
-
-    # Combinig attributes into single listof tuples
+    # Combinando atributos de listas
     features=list(zip(x_encoded))
 
     # fit the model
@@ -33,28 +32,5 @@ def arbolDecision_func(x_name, y_name, titulo, datos):
     plot_tree(clf, filled=True)
     plt.savefig("./static/images/arbol.png") 
     # plt.show()
-    print(titulo)
     return {'informacion': "Informacion"}
-    
-
-def arbolDecision_func2(x_name, y_name, datos):
-    x = datos[x_name].values.reshape(-1,1)
-    y = datos[y_name].values.reshape(-1,1)
-    # Creating labelEncoder
-    le = preprocessing.LabelEncoder()
-
-    # Converting string labels into numbers.
-    outlook_encoded=le.fit_transform(x)
-    label=le.fit_transform(y)
-
-    print ("outlook:  ",outlook_encoded)
-
-    # Combinig attributes into single listof tuples
-    features=list(zip(outlook_encoded))
-    print (features)
-
-    # fit the model
-    clf = DecisionTreeClassifier().fit(features, label)
-    plot_tree(clf, filled=True)
-    plt.show()
 
